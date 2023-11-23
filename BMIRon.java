004945d247bc56f2bdc986640e96b9d002cc2926
irondjerwi <151853922+irondjerwi@users.noreply.github.com>
2023-11-23 21:32:17 +0700

    Add files via upload

---
+++ BMIRon.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class BMIRon {
+    public static void main(String[] args){
+        int beratbadan = 53;
+        float tinggibadan =1.70f;
+        float bmi = (beratbadan/(tinggibadan*tinggibadan));
+        
+        System.out.println("BMI SAYA ADALAH\t:"+bmi);
+        
+        if (bmi<18.5){
+           System.out.println("BERAT BADAN SAYA KURANG");
+        }
+        else if (bmi<24.9){
+            System.out.println("BERAT BADAN SAYA NORMAL/IDEAL");
+        }
+        else if (bmi<29.9){
+            System.out.println("BERAT BADAN SAYA LEBIH");
+        }
+        else {
+            System.out.println("OBESITAS");
+        }
+    }
+}
+    
+
